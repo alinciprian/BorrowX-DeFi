0a46e7b3c4dbfd5e4c0bfedbe453a1ca509ebcb5
Ciprian Eniță <ciprian_enita@yahoo.com>
2025-03-20 19:31:16 +0200

    test: added test

--- contracts/test/unit/concrete/mintxUSDC/mintxUSDC.t.sol
+++ contracts/test/unit/concrete/mintxUSDC/mintxUSDC.t.sol
@@ -10,6 +10,7 @@ import {MockV3Aggregator} from "../../../mock/MockV3Aggregator.sol";
 
 contract depositCollateralTest is Base_Test {
     uint256 depositAmount = 1 ether;
+    uint256 xUSDCMintAmount = 900e18;
 
     function setUp() public virtual override {
         Base_Test.setUp();
@@ -19,61 +20,55 @@ contract depositCollateralTest is Base_Test {
     ///mintxUSDC
     //////////////////////////
 
-    function testItShouldRevertIfMintAmountIsZero() public {
+    function test_RevertWhen_MintAmountIsZero() public {
+        // Make bob the caller of the function
         vm.startPrank(users.bob);
 
+        // Expect the next call to revert with the {BorrowX__NeedsMoreThanZero} error
         vm.expectRevert(BorrowX.BorrowX__NeedsMoreThanZero.selector);
-        borrowXContract.mintxUSDC(0);
 
-        vm.stopPrank();
+        // Run the test
+        borrowXContract.mintxUSDC(0);
     }
 
-    function testItShouldRevertIfMintingExceedsLTV() public {
+    function test_RevertWhen_MintAmountBreaksLTV() public {
+        // Make bob the caller of the function
         vm.startPrank(users.bob);
 
+        // Bob deposits 1 ether = 2_000 USD so he should be allowed to mint max 1_000 xUSDC
+        borrowXContract.depositCollateral{value: depositAmount}();
+
+        // Expect the next call to revert with the {BorrowX__ExceedsLoanToValue} error since bob tries to mint more than 1_001 xUSDC
         vm.expectRevert(BorrowX.BorrowX__ExceedsLoanToValue.selector);
-        borrowXContract.mintxUSDC(10e18);
 
-        vm.stopPrank();
+        // Run the test
+        borrowXContract.mintxUSDC(1001e18);
     }
 
-    function testItShouldUpdateStorageAfterMinting() public {
-        vm.deal(users.eve, 1 ether);
-        vm.startPrank(users.eve);
+    function test_MintXUSDC() public {
+        // Make bob caller of the function
+        vm.startPrank(users.bob);
 
-        borrowXContract.depositCollateral{value: depositAmount}();
-        borrowXContract.mintxUSDC(900e18);
-
-        uint256 aliceMinted = borrowXContract.getUserMintedXUSDC(users.eve);
-
-        assertEq(aliceMinted, 900e18);
-
-        vm.stopPrank();
-    }
-
-    function testItShouldTransferFundsAfterMinting() public {
-        vm.deal(users.eve, 1 ether);
-        vm.startPrank(users.eve);
-
-        borrowXContract.depositCollateral{value: depositAmount}();
-        borrowXContract.mintxUSDC(900e18);
-
-        uint256 aliceBalance = xUSDCContract.balanceOf(users.eve);
-        assertEq(aliceBalance, 900e18);
-
-        vm.stopPrank();
-    }
-
-    function testItShouldEmitEventAfterMinting() public {
-        vm.deal(users.eve, 1 ether);
-        vm.startPrank(users.eve);
-
+        // bob deposits 1 ether so he is able to mint max 1000 xUSDC
         borrowXContract.depositCollateral{value: depositAmount}();
 
+        // Expect the {collateralDeposited} event to be emitted
         vm.expectEmit();
-        emit BorrowX.xUSDCMinted(users.eve, 900e18);
-        borrowXContract.mintxUSDC(900e18);
+        emit BorrowX.xUSDCMinted(users.bob, xUSDCMintAmount);
 
-        vm.stopPrank();
+        // Run the test
+        borrowXContract.mintxUSDC(xUSDCMintAmount);
+
+        // We store the amount of xUSDC bob minted according to the storage
+        uint256 bobMinted = borrowXContract.getUserMintedXUSDC(users.bob);
+
+        // We store bob's xUSDC balance from the xUSDC contract
+        uint256 bobXUSDCBalance = xUSDCContract.balanceOf(users.bob);
+
+        // Assert the storage is updated according to the amount minted
+        assertEq(bobMinted, xUSDCMintAmount);
+
+        // Assert xUSDC has been transfered to bob
+        assertEq(bobXUSDCBalance, xUSDCMintAmount);
     }
 }
