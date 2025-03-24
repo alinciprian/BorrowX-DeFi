38878e03e62d06f692155663b918c8fd0a470abb
Ciprian Eniță <ciprian_enita@yahoo.com>
2025-03-24 16:56:34 +0200

    add connect wallet page

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -1,44 +1,13 @@
 "use client";
 
-import { useAccount, useDisconnect } from "wagmi";
-import { usePrivy, useWallets } from "@privy-io/react-auth";
-import { useSetActiveWallet } from "@privy-io/wagmi";
-import Balance from "../components/Balance";
-
-import wagmiPrivyLogo from "../public/wagmi_privy_logo.png";
-
-const MonoLabel = ({ label }: { label: string }) => {
-  return (
-    <span className="rounded-xl bg-slate-200 px-2 py-1 font-mono">{label}</span>
-  );
-};
+import LoginPage from "../components/LoginPage";
+import { useAccount } from "wagmi";
 
 export default function Home() {
-  // Privy hooks
-  const {
-    ready,
-    user,
-    authenticated,
-    login,
-    connectWallet,
-    logout,
-    linkWallet,
-  } = usePrivy();
-
-  const { wallets, ready: walletsReady } = useWallets();
-
-  // WAGMI hooks
-  const { address, isConnected, isConnecting, isDisconnected } = useAccount();
-  const { disconnect } = useDisconnect();
-  const { setActiveWallet } = useSetActiveWallet();
-
-  if (!ready) {
-    return null;
-  }
-
+  const { isConnected } = useAccount();
   return (
-    <div>
-      <Balance />
-    </div>
+    <>
+      <LoginPage />
+    </>
   );
 }
