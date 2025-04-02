de23e7ac8ab5999d8e90bbb90e7fbef7ae84fb13
alinciprian <ciprian_enita@yahoo.com>
2025-04-02 09:03:35 +0300

    chore: update contracts addresses

--- frontend/lib/constants.ts
+++ frontend/lib/constants.ts
@@ -1,2 +1,2 @@
-export const BorrowXAddress = "0x7ACC45Ed7b25AED601Bf2b0880b865E7B8BdF7D2";
-export const xUSDCAddress = "0xBEed2827e2cb03ea4B4d4DA8A1CF8638D9CeCA27";
+export const BorrowXAddress = "0x752175326E6922e55D49AFc47C8810bD1355d2bD";
+export const xUSDCAddress = "0x965b33d5d7f52856Ab7cf4c7c68B06BB8eEfEb92";
