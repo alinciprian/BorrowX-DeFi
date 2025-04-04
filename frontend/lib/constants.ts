248c89c766b63418afc70148935080de162801d9
alinciprian <ciprian_enita@yahoo.com>
2025-04-04 15:02:02 +0300

    refactor: thi commit will change the contract addresses

--- frontend/lib/constants.ts
+++ frontend/lib/constants.ts
@@ -1,2 +1,2 @@
-export const BorrowXAddress = "0x752175326E6922e55D49AFc47C8810bD1355d2bD";
-export const xUSDCAddress = "0x965b33d5d7f52856Ab7cf4c7c68B06BB8eEfEb92";
+export const BorrowXAddress = "0x4bB6C572Bed1A41049Fd9274C329A0F49A367050";
+export const xUSDCAddress = "0x36be8C1E7Ac0EC9F58A8417e6aD3999446816147";
