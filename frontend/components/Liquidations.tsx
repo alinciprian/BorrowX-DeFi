927e8e12480864469ab8dac3553d39ab8e3c88c8
alinciprian <ciprian_enita@yahoo.com>
2025-04-14 14:33:22 +0300

    feat: add fetchPositions methos

--- frontend/components/Liquidations.tsx
+++ frontend/components/Liquidations.tsx
@@ -1,6 +1,37 @@
 import { Card, CardHeader, CardTitle, CardContent } from "@/components/ui/card";
 
 export default function Liquidations() {
+  const fetchUserPosition = async () => {
+    const endpoint = "http://localhost:8080/v1/graphql";
+    const headers = {
+      "content-type": "application/json",
+    };
+    const graphqlQuery = {
+      query: `query UserPosition {
+            Position {
+              id
+              borrowed
+              collateral
+              account
+              timestamp
+              txHash
+            }
+          }`,
+    };
+
+    const options = {
+      method: "POST",
+      headers: headers,
+      body: JSON.stringify(graphqlQuery),
+    };
+
+    const response = await fetch(endpoint, options);
+    const data = await response.json();
+
+    const position = data.data.Position;
+    console.log(position); // data
+  };
+
   return (
     <div className="flex flex-col items-center justify-center h-screen bg-black text-white relative">
       {/* <div className="absolute -top-15 left-2 text-[10px] font-semibold">
