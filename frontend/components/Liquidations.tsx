7d87581adef3b6772ba0fb6b74dfbeadf4b42953
alinciprian <ciprian_enita@yahoo.com>
2025-04-14 14:31:47 +0300

    feat: add card component for liquidation page

--- frontend/components/Liquidations.tsx
+++ frontend/components/Liquidations.tsx
@@ -1,7 +1,22 @@
+import { Card, CardHeader, CardTitle, CardContent } from "@/components/ui/card";
+
 export default function Liquidations() {
   return (
-    <div>
-      <p>This is the liquidation page</p>
+    <div className="flex flex-col items-center justify-center h-screen bg-black text-white relative">
+      {/* <div className="absolute -top-15 left-2 text-[10px] font-semibold">
+        <p className="text-gray-400">Net worth:</p>
+        <div className="flex items-center text-white">
+          <p className="text-gray-400">$ </p>
+          <p> {netWorth?.formatted}</p>
+        </div>
+      </div> */}
+
+      <Card className="w-[400px] bg-gray-800 text-white">
+        <CardHeader>
+          <CardTitle>Liquidations</CardTitle>
+        </CardHeader>
+        <CardContent></CardContent>
+      </Card>
     </div>
   );
 }
