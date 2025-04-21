47697ba55b565290fd932cd40c21e2db4701f404
Ciprian Eniță <ciprian_enita@yahoo.com>
2025-04-21 19:58:01 +0300

    chore: remove unused import

--- frontend/app/liquidations/page.tsx
+++ frontend/app/liquidations/page.tsx
@@ -4,19 +4,16 @@ import Navbar from "../../components/Navbar";
 import LoginPage from "../../components/LoginPage";
 
 import { useAccount } from "wagmi";
-import { useState } from "react";
 import Liquidations from "@/components/Liquidations";
 
 export default function Dashboard() {
   const { isConnected } = useAccount();
 
-  const [isLoading, setIsLoading] = useState<boolean>(false);
-
   return (
     <>
       {isConnected ? (
         <div>
-          <Navbar /> <Liquidations  />
+          <Navbar /> <Liquidations />
         </div>
       ) : (
         <LoginPage />
